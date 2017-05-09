f045e3b9da16bf2a5716e327789814ef86b6fc8a
sassc <sassc@172.22.57.49>
2017-05-09 16:54:49 +0200

    Inizializzazione - Interfaccia

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -1,5 +1,55 @@
 package it.polito.tdp.metrodeparis;
 
+import java.net.URL;
+import java.util.*;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.metrodeparis.model.Fermata;
+import it.polito.tdp.metrodeparis.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class MetroDeParisController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Fermata> boxPartenza;
+
+    @FXML
+    private ComboBox<Fermata> boxArrivo;
+
+    @FXML
+    private Button btnPercorso;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doPercorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxPartenza != null : "fx:id=\"boxPartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert boxArrivo != null : "fx:id=\"boxArrivo\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert btnPercorso != null : "fx:id=\"btnPercorso\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+
+    }
+    Model model;
+	public void setModel(Model model) {
+		this.model=model;
+		List<Fermata> fermate=model.getTutteFermate();
+		boxPartenza.getItems().addAll(fermate);
+		boxArrivo.getItems().addAll(fermate);
+	}
 }
--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
@@ -1,5 +1,18 @@
 package it.polito.tdp.metrodeparis.model;
 
-public class Model {
+import java.util.*;
 
+import it.polito.tdp.metrodeparis.dao.MetroDAO;
+
+public class Model {
+	
+	private List<Fermata> listaFermate;
+	
+	public List<Fermata> getTutteFermate(){
+		MetroDAO dao=new MetroDAO();
+		if(listaFermate==null){
+			listaFermate=dao.getAllFermate();
+		}
+		return listaFermate;
+	}
 }
